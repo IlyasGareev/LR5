32b54f4ffbf681272d9c414357cd66bdbc56280c
IlyasGareev <gareev_ilyas@list.ru>
2022-01-23 14:25:06 +0500

    Реализация

--- LR5/LR5.cpp
+++ LR5/LR5.cpp
@@ -111,3 +111,81 @@ Base& func_3(Base& obj) {
     printf("Base& func_3(Base& obj)\n");
     return obj;
 }
+int main()
+{
+    srand(time(NULL));
+
+    Dog* l = new Dog();             //объект класса наследника
+    l->foo1();                      //вызов перекрываемого метода у объекта класса наследника 
+    l->foo2();                      //вызов наследуемого метода у объкта класса наследника 
+    delete l;
+
+
+    Animal* m = new Animal();          //объект базового класса
+    m->foo3();                       //вызов виртуального метода у объекта базового класса
+    delete m;
+
+    Animal* n = new Dog();              //объект класса наследника, помещенного в переменную базового класса
+    n->foo3();                          //вызов наследуемого виртуального метода
+    delete n;
+
+    printf("\n\n");
+
+    Animal* a = new Dog();
+    if (a->classname() == "Dog") {
+        ((Dog*)a)->run();
+    }
+    delete a;
+
+    Animal* b = new Dog();
+    if (b->isA("Dog")) {
+        ((Dog*)b)->run();
+    }
+    delete b;
+
+    Animal* c = new Dog();
+    ((Dog*)c)->run();                      //опасное приведение типов
+    delete c;
+
+    Animal* d = new Dog();
+    if (dynamic_cast<Dog*>(d)) {             //безопасное приведение типов
+        ((Dog*)d)->run();
+    }
+    delete d;
+
+    printf("\n\n");
+
+    Base* w = new Base();
+    func1(w);
+    func2(w);
+    func3(*w);
+
+    printf("\n\n");
+
+    Desc* x = new Desc();
+    func1(x);
+    func2(x);
+    func3(*x);
+    delete x;
+
+    printf("\n\n");
+
+    func_1(*w);
+    func_2(w);
+    func_3(*w);
+    delete w;
+
+    printf("\n\n");
+
+    {
+        unique_ptr<Base> unq(new Desc());
+    }
+
+    printf("\n\n");
+
+    Base* z = new Desc();
+    shared_ptr<Base> ptr1(z);
+    {
+        shared_ptr<Base> ptr2(ptr1);
+    }
+}
